77a891835c8ff70367766e221eb5a783f7e0382c
RinYato <chearithorn@gmail.com>
2024-09-18 22:30:56 +0700

    ci: add start:api

--- apps/api/src/lib/tasker.ts
+++ apps/api/src/lib/tasker.ts
@@ -1,29 +1,29 @@
 import type { App } from "@/setup/context";
-import { createBullBoard } from "@bull-board/api";
-import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
-import { HonoAdapter } from "@bull-board/hono";
-import { Queue, type Worker } from "bullmq";
+// import { createBullBoard } from "@bull-board/api";
+// import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
+// import { HonoAdapter } from "@bull-board/hono";
+// import { Queue, type Worker } from "bullmq";
 import { serveStatic } from "hono/bun";
 
 export function registerTasker(app: App, taskers: Tasker[]) {
   // start all taskers
   taskers.forEach((tasker) => tasker.start());
 
-  const serverAdapter = new HonoAdapter(serveStatic);
+  // const serverAdapter = new HonoAdapter(serveStatic);
 
-  createBullBoard({
-    queues: taskers.map(createBullAdapter),
-    serverAdapter,
-  });
+  // createBullBoard({
+  //   queues: taskers.map(createBullAdapter),
+  //   serverAdapter,
+  // });
 
-  serverAdapter.setBasePath("/queue");
+  // serverAdapter.setBasePath("/queue");
 
-  app.route("/queue", serverAdapter.registerPlugin());
+  // app.route("/queue", serverAdapter.registerPlugin());
 }
 
-function createBullAdapter(tasker: Tasker) {
-  return new BullMQAdapter(new Queue(tasker.worker.name));
-}
+// function createBullAdapter(tasker: Tasker) {
+//   return new BullMQAdapter(new Queue(tasker.worker.name));
+// }
 
 export const DEFAULT_CONNECTION = {
   host: "127.0.0.1",
