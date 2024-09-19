ecc92460ca56660ed277b071a5b587786bc9b274
RinYato <chearithorn@gmail.com>
2024-09-19 14:47:37 +0700

    fix: change cors

--- apps/api/src/setup/cors.ts
+++ apps/api/src/setup/cors.ts
@@ -3,5 +3,5 @@ import type { App } from "./context";
 import { cors } from "hono/cors";
 
 export function registerCors(app: App) {
-  app.use("*", cors({ origin: [env.API_URL], credentials: true }));
+  app.use("*", cors({ origin: [env.WEB_URL], credentials: true }));
 }
