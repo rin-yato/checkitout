0887000aa8921bf57b1a6c82b20715f74ab4875e
RinYato <chearithorn@gmail.com>
2024-10-01 11:48:04 +0700

    docs: add early stage dev banner

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -3,6 +3,8 @@ import "./global.css";
 import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
+import { Banner } from "fumadocs-ui/components/banner";
+import { Info } from "lucide-react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -43,6 +45,11 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={cn(inter.className, "antialiased")} suppressHydrationWarning>
       <body>
+        <Banner variant="rainbow" changeLayout={false} id="early-stage-warning" className="">
+          <Info size={18} className="mr-2" />
+          Checkitout is still in early development, API are prone to change. Please report any
+          issues you find.
+        </Banner>
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
